e7261cda4813157b21ebcce91787c2d5a49fd581
Duc Cao <caovanducs@gmail.com>
2021-05-05 12:32:12 +0700

    Handled Notify Admin Answer By Email

--- utils/gmail.js
+++ utils/gmail.js
@@ -1,16 +1,17 @@
 const nodemailer = require("nodemailer");
-const dotenv = require('dotenv');
-
+const dotenv = require("dotenv");
 
 dotenv.config();
+
+const user = `${process.env.email}`;
 
 let transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
   auth: {
-    user: process.env.email,
-    pass: process.env.password,
+    user: `${process.env.email}`,
+    pass: `${process.env.password}`,
   },
   tls: {
     rejectUnauthorized: false,
@@ -18,13 +19,29 @@ let transporter = nodemailer.createTransport({
 });
 
 module.exports = {
-  noTifyToAdmin: (admin_email, que_title, quetion_or_answer) => {
+  noTifyToAdmin: (admin_email, que_title, question) => {
     const options = {
       from: `mini-q2a - UDPT04 - API Service - ${user}`,
       to: `${admin_email}`,
       subject: `Question ${que_title} was accepted!`,
-      text: `Question Accepted Information\n${JSON.stringify(
-        quetion_or_answer
+      text: `Question ${que_title} was accepted!\nQuestion Accepted Information\n${JSON.stringify(
+        question
+      )}`,
+    };
+
+    transporter.sendMail(options, (er, info) => {
+      if (er) console.log(er);
+      else console.log(info);
+    });
+  },
+
+  notifyAnswerToAdmin: (admin_email, number, answer) => {
+    const options = {
+      from: `mini-q2a - UDPT04 - API Service - ${user}`,
+      to: `${admin_email}`,
+      subject: `There are ${number} answer was accepted!`,
+      text: `There are ${number} answer was accepted!\nAnswer accepted information\n${JSON.stringify(
+        answer
       )}`,
     };
 
